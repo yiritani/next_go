83953151e8d13610250bb929d175bf59a9a086b3
yusuke <yusuke.iritani0909@gmail.com>
2024-07-01 20:43:00 +0900

    バックエンドと通信可能

--- apps/backend/main.go
+++ apps/backend/main.go
@@ -12,7 +12,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:3001"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
