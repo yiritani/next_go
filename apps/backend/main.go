756c44fa12d54430e904b746547d5bcd937986ad
yusuke <yusuke.iritani0909@gmail.com>
2024-07-04 23:23:55 +0900

    DB繋がらない

--- apps/backend/main.go
+++ apps/backend/main.go
@@ -2,33 +2,40 @@ package main
 
 import (
 	"context"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"net/http"
+	"os"
 	"time"
-
-	"github.com/jackc/pgx/v5"
-	"tutorial.sqlc.dev/app/src/sqlc"
+	db "tutorial.sqlc.dev/app/src/sqlc"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
+var pool *pgxpool.Pool
+
 func dbSel(c *gin.Context) {
-	ctx := context.Background()
+	q := db.New(pool)
+	resultSet, err := q.GetSystemUsers(context.Background())
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
 
-	conn, _ := pgx.Connect(ctx, "user=postgres dbname=public sslmode=verify-full")
-	defer conn.Close(ctx)
-
-	queries := sqlc.New(conn)
-
-	system, _ := queries.ListSystems(ctx)
-	println(system)
-
-	c.JSON(http.StatusOK, gin.H{
-		"message": "pong",
-	})
+	c.JSON(http.StatusOK, resultSet)
 }
 
 func main() {
+	connString := "user=postgres password=password host=localhost port=5432 dbname=public sslmode=disable"
+	pool, err := pgxpool.New(context.Background(), connString)
+	if err != nil {
+		panic(err)
+		os.Exit(1)
+	}
+	defer pool.Close()
+
 	r := gin.Default()
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:3001"},
