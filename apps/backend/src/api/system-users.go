e770a63acdaa63376669e2f5ee57a9ffef964fdd
yusuke <yusuke.iritani0909@gmail.com>
2024-07-14 22:03:59 +0900

    a

--- apps/backend/src/api/system-users.go
+++ apps/backend/src/api/system-users.go
@@ -4,12 +4,10 @@ import (
 	"context"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"tutorial.sqlc.dev/app/src/lib"
 	"tutorial.sqlc.dev/app/src/sqlc"
 )
 
 func (server *Server) listSystems(c *gin.Context) {
-	lib.Msging("getSystemUsers")
 	resultSet, err := server.Queries.ListSystems(context.Background(), sqlc.ListSystemsParams{
 		Limit:  10,
 		Offset: 0,
