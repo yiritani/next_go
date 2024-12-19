5c0a64601729b71fa98839561590248aa3e38aca
yusuke <yusuke.iritani0909@gmail.com>
2024-12-19 23:28:51 +0900

    declare env

--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -3,9 +3,9 @@ import {useState} from "react";
 export default function Home() {
   const [fetchedData, setFetchedData] = useState(null);
   const fetchPing = async () => {
-    console.log(`${process.env.NEXT_PUBLIC_API_URL}/ping`)
-    // const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/ping`);
-    const res = await fetch(`https://next-go-backend-1063239685310.us-central1.run.app/ping`);
+    console.log('%o', process.env.NEXT_PUBLIC_API_URL);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/ping`);
+    // const res = await fetch(`https://next-go-backend-1063239685310.us-central1.run.app/ping`);
     const data = await res.json();
     console.log(data);
     setFetchedData(data.message);
