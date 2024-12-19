d85e777babe1a7ad04df6713509bc9ffefd6db0d
yusuke <yusuke.iritani0909@gmail.com>
2024-12-20 03:51:39 +0900

    infra

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.34.0"
+    }
+  }
+}
 provider "google" {
   project = var.project_id
   region  = var.region
@@ -12,5 +20,16 @@ resource "google_service_account" "cloudbuild_service_account" {
 resource "google_project_iam_member" "logs_logging_writer" {
   project = var.project_id
   role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
+}
+
+resource "google_project_iam_member" "artifact_registry_writer" {
+  project = var.project_id
+  role   = "roles/artifactregistry.writer"
+  member = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
+}
+resource "google_project_iam_member" "artifact_registry_reader" {
+  project = var.project_id
+  role    = "roles/artifactregistry.reader"
   member  = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
 }
