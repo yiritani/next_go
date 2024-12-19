41611b76edf53b775e3eb4cac07951be258c4c2a
yusuke <yusuke.iritani0909@gmail.com>
2024-12-20 02:24:54 +0900

    infra

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -27,7 +27,6 @@ resource "google_storage_bucket" "logs_bucket" {
 
 resource "google_storage_bucket_iam_binding" "logs_bucket_writer" {
   bucket = google_storage_bucket.logs_bucket.name
-
   role = "roles/storage.objectAdmin"
 
   members = [
