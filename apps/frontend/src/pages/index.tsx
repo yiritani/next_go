8f5d851a15c7ff819cce2581dab62f0f8bfe33e1
yusuke <yusuke.iritani0909@gmail.com>
2024-12-20 12:18:54 +0900

    backend作成

--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   const fetchPing = async () => {
     console.log('%o', process.env.NEXT_PUBLIC_API_URL);
     // const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/ping`);
-    const res = await fetch(`https://next-go-cloudrun-frontend-d6cnpggbya-uc.a.run.app/ping`);
+    const res = await fetch(`https://next-go-cloudrun-backend-1063239685310.us-central1.run.app/ping`);
     const data = await res.json();
     console.log(data);
     setFetchedData(data.message);
