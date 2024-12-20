0a6935a44529cfd1ebfd1c28e7bf16a9af8a4fda
yusuke <yusuke.iritani0909@gmail.com>
2024-12-20 11:27:22 +0900

    docker push

--- infra/terraform/frontend.tf
+++ infra/terraform/frontend.tf
@@ -28,25 +28,25 @@ resource "google_cloudbuild_trigger" "frontend" {
   filename = "cloudbuild.frontend.yaml"
 }
 
-# resource "google_cloud_run_service" "frontend" {
-#   name     = "${var.service_name}-frontend"
-#   location = var.region
-#
-#   template {
-#     spec {
-#       containers {
-#         image = "${var.region}-docker.pkg.dev/${var.project_id}/${var.image_repo_frontend}:latest"
-#         env {
-#           name  = "NEXT_PUBLIC_API_URL"
-#           value = "https://backend-service-${var.region}.a.run.app"
-#         }
-#       }
-#     }
-#   }
-#
-#   metadata {
-#     annotations = {
-#       "run.googleapis.com/client-name" = "terraform"
-#     }
-#   }
-# }
+resource "google_cloud_run_service" "frontend" {
+  name     = "${var.service_name}-frontend"
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = "${var.region}-docker.pkg.dev/${var.project_id}/${var.image_repo}/frontend:latest"
+        env {
+          name  = "NEXT_PUBLIC_API_URL"
+          value = "https://backend-service-${var.region}.a.run.app"
+        }
+      }
+    }
+  }
+
+  metadata {
+    annotations = {
+      "run.googleapis.com/client-name" = "terraform"
+    }
+  }
+}
