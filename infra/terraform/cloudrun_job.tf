e73b79861892bc423d636b7f18d13a41600f6e4a
yusuke <yusuke.iritani0909@gmail.com>
2024-12-20 23:24:36 +0900

    job app

--- infra/terraform/cloudrun_job.tf
+++ infra/terraform/cloudrun_job.tf
@@ -7,6 +7,10 @@ resource "google_cloud_run_v2_job" "job" {
       service_account = google_service_account.cloudrun_service_account.email
       containers {
         image = "${var.region}-docker.pkg.dev/${var.project_id}/${var.image_repo}/job:latest"
+        env {
+          name  = "NEXT_PUBLIC_API_URL"
+          value = "https://backend-service-${var.region}.a.run.app"
+        }
       }
     }
   }
