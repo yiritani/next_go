522c1a219e71902d54539b210c0a30d9c8151095
yusuke <yusuke.iritani0909@gmail.com>
2024-12-20 22:58:16 +0900

    job app

--- infra/terraform/cloudbuild.tf
+++ infra/terraform/cloudbuild.tf
@@ -1,3 +1,53 @@
+resource "google_cloudbuild_trigger" "backend" {
+  name = "${var.service_name}-build-trigger-backend"
+
+  substitutions = {
+    _IMAGE      = "${var.region}-docker.pkg.dev/${var.project_id}/${var.image_repo}/backend"
+    _REGION     = var.region
+    _SERVICE    = var.service_name
+    _DOCKERFILE = var.dockerfile_backend
+    _REPO       = var.image_repo
+    _CLOUD_RUN_SERVICE = google_cloud_run_service.backend.name
+    _CLOUD_RUN_SERVICE_ACCOUNT = google_service_account.cloudrun_service_account.email
+  }
+
+  github {
+    owner = "yiritani"
+    name  = "next_go"
+    push {
+      branch = "main"
+    }
+  }
+
+  service_account = google_service_account.cloudbuild_service_account.id
+  filename = "cloudbuild.backend.yaml"
+}
+
+resource "google_cloudbuild_trigger" "frontend" {
+  name = "${var.service_name}-build-trigger-frontend"
+
+  substitutions = {
+    _IMAGE      = "${var.region}-docker.pkg.dev/${var.project_id}/${var.image_repo}/frontend"
+    _REGION     = var.region
+    _SERVICE    = var.service_name
+    _DOCKERFILE = var.dockerfile_frontend
+    _REPO       = var.image_repo
+    _CLOUD_RUN_SERVICE = google_cloud_run_service.frontend.name
+    _CLOUD_RUN_SERVICE_ACCOUNT = google_service_account.cloudrun_service_account.email
+  }
+
+  github {
+    owner = "yiritani"
+    name  = "next_go"
+    push {
+      branch = "main"
+    }
+  }
+
+  service_account = google_service_account.cloudbuild_service_account.id
+  filename = "cloudbuild.frontend.yaml"
+}
+
 resource "google_cloudbuild_trigger" "job" {
   name = "${var.service_name}-build-trigger-job"
 
@@ -7,7 +57,7 @@ resource "google_cloudbuild_trigger" "job" {
       _SERVICE    = var.service_name
       _DOCKERFILE = var.dockerfile_job
       _REPO       = var.image_repo
-      # _CLOUD_RUN_SERVICE = google_cloud_run_v2_job.job.name
+      _CLOUD_RUN_SERVICE = google_cloud_run_v2_job.job.name
       _CLOUD_RUN_SERVICE_ACCOUNT = google_service_account.cloudrun_service_account.email
   }
 
@@ -22,3 +72,4 @@ resource "google_cloudbuild_trigger" "job" {
   service_account = google_service_account.cloudbuild_service_account.id
   filename = "cloudbuild.job.yaml"
 }
+
--- infra/terraform/cloudrun_job.tf
+++ infra/terraform/cloudrun_job.tf
@@ -1,12 +1,12 @@
-# resource "google_cloud_run_v2_job" "job" {
-#   name     = "${var.service_name}-cloudrun-job"
-#   location = var.region
-#
-#   template {
-#     template {
-#       containers {
-#         image = "${var.region}-docker.pkg.dev/${var.project_id}/${var.image_repo}/job:latest"
-#       }
-#     }
-#   }
-# }
+resource "google_cloud_run_v2_job" "job" {
+  name     = "${var.service_name}-cloudrun-job"
+  location = var.region
+
+  template {
+    template {
+      containers {
+        image = "${var.region}-docker.pkg.dev/${var.project_id}/${var.image_repo}/job:latest"
+      }
+    }
+  }
+}
