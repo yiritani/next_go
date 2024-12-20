09d66df8e0dceafb4a5e4097dd3e43e021e8f81b
yusuke <yusuke.iritani0909@gmail.com>
2024-12-21 00:19:02 +0900

    job app

--- infra/terraform/cloudrun_job.tf
+++ infra/terraform/cloudrun_job.tf
@@ -10,7 +10,7 @@ resource "google_cloud_run_v2_job" "job" {
         image = "${var.region}-docker.pkg.dev/${var.project_id}/${var.image_repo}/job:latest"
         env {
           name  = "NEXT_PUBLIC_API_URL"
-          value = google_cloud_run_service.backend.status[0].url
+          value = "https://next-go-cloudrun-backend-1063239685310.us-central1.run.app"
         }
       }
     }
