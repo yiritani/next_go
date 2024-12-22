ea87e13f303e614292fd4f3c82f391af9f1d52e3
yusuke <yusuke.iritani0909@gmail.com>
2024-12-22 21:11:56 +0900

    GHA

--- infra/terraform/development/iam.tf
+++ infra/terraform/development/iam.tf
@@ -1,4 +1,9 @@
-resource "google_service_account" "cloudbuild_service_account" {
+resource "google_project_service" "enable_iam_credentials_api" {
+    project = var.project_id
+    service = "iamcredentials.googleapis.com"
+}
+
+  resource "google_service_account" "cloudbuild_service_account" {
   account_id   = "cloudbuild-sa"
   display_name = "cloudbuild-sa"
   description  = "Cloud build service account"
