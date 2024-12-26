6d3a3b9c144ba9d7f3f1888b8c81c42e0ef39db8
yusuke <yusuke.iritani0909@gmail.com>
2024-12-27 01:57:50 +0900

    atlantis

--- infra/terraform/atlantis/main.tf
+++ infra/terraform/atlantis/main.tf
@@ -1,31 +1,5 @@
-resource "google_compute_network" "default" {
-  name                    = "example-network"
-  auto_create_subnetworks = false
-  project                 = "next-go-445902"
-}
-
-# cloud run
-resource "google_cloud_run_service" "default" {
-  name     = "example-service"
-  location = "us-central1"
-  project  = "next-go-445902"
-
-  template {
-    spec {
-      containers {
-        image = "gcr.io/cloudrun/hello"
-      }
-    }
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
+# resource "google_compute_network" "default" {
+#   name                    = "example-network"
+#   auto_create_subnetworks = false
+#   project                 = "next-go-445902"
+# }
