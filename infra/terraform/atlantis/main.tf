cf28a0710041039ff80d1bb1ee3d3ba9d1166980
yusuke <yusuke.iritani0909@gmail.com>
2024-12-27 13:47:00 +0900

    atlantis

--- infra/terraform/atlantis/main.tf
+++ infra/terraform/atlantis/main.tf
@@ -1,5 +1,5 @@
 resource "google_compute_network" "default" {
-  name                    = "example-network"
+  name                    = "example-network2"
   auto_create_subnetworks = false
   project                 = "next-go-445902"
 }
