25bce184e087811bffd55844921203a66bad31a3
yusuke <yusuke.iritani0909@gmail.com>
2024-12-27 14:48:35 +0900

    atlantis

--- infra/terraform/development/cloudrun.tf
+++ infra/terraform/development/cloudrun.tf
@@ -77,7 +77,7 @@ resource "google_cloud_run_service_iam_policy" "noauth-backend" {
 }
 
 resource "google_cloud_run_service" "frontend" {
-  name     = "${var.service_name}-cloudrun-frontend2"
+  name     = "${var.service_name}-cloudrun-frontend"
   location = var.region
 
   template {
