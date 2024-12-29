b152be2372874d99df1d75bd0c8ab4005d453074
yusuke <yusuke.iritani0909@gmail.com>
2024-12-29 23:01:04 +0900

    frontend: flex

--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -19,10 +19,10 @@ export default function Home() {
   return (
     <div className="flex flex-col h-screen">
       {error && <p className="text-red-500 text-center">Error loading data</p>}
-      <div className="flex-1 bg-red-200 flex items-start justify-start pl-10 pt-10">
+      <div className="flex-2 bg-red-200 flex items-start justify-start pl-10 pt-10">
         <Ping />
       </div>
-      <div className="flex-1 bg-green-200 flex items-start justify-start pl-10 pt-10">
+      <div className="flex-2 bg-green-200 flex items-start justify-start pl-10 pt-10">
         <Users users={users} />
       </div>
       <div className="flex-1 bg-blue-200 flex items-start justify-start pl-10 pt-10">
