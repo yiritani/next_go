e2b00d91d032867bcde6fb695784bdf5805780a0
yusuke <yusuke.iritani0909@gmail.com>
2024-12-30 00:31:22 +0900

    deploy

--- infra/terraform/development/vpc.tf
+++ infra/terraform/development/vpc.tf
@@ -1,20 +1,33 @@
-# resource "google_project_service" "enable_vpc_access_api" {
-#   project = var.project_id
-#   service = "vpcaccess.googleapis.com"
-# }
-# resource "google_compute_network" "vpc_network" {
-#   name                    = var.vpc_network
-#   auto_create_subnetworks = false
-# }
-# # VPC Connector
-# resource "google_vpc_access_connector" "vpc_connector" {
-#   name               = "${var.service_name}-connector"
-#   region             = var.region
-#   network       = google_compute_network.vpc_network.id
-#   ip_cidr_range = "10.8.0.0/28"
-#   machine_type       = "e2-micro"
-#   max_throughput     = 300
-#   min_throughput     = 200
-#
-#   depends_on = [google_project_service.enable_vpc_access_api]
-# }
+resource "google_project_service" "vpc_api" {
+  service = "vpcaccess.googleapis.com"
+  project = var.project_id
+}
+
+# VPCネットワーク作成
+resource "google_compute_network" "vpc_network" {
+  depends_on = [google_project_service.vpc_api]
+
+  name                    = "${var.service_name}-vpc-network"
+  auto_create_subnetworks = false
+}
+
+# サブネット作成
+resource "google_compute_subnetwork" "vpc_subnet" {
+  depends_on = [google_project_service.vpc_api]
+
+  name          = "${var.service_name}-vpc-subnet"
+  ip_cidr_range = "10.0.0.0/24"
+  region        = var.region
+  network       = google_compute_network.vpc_network.id
+}
+
+# VPCアクセスコネクタ作成
+resource "google_vpc_access_connector" "vpc_connector" {
+  depends_on = [google_project_service.vpc_api]
+
+  name   = "${var.service_name}-vpc-connector"
+  region = var.region
+  network = google_compute_network.vpc_network.id
+
+  ip_cidr_range = "10.8.0.0/28"
+}
