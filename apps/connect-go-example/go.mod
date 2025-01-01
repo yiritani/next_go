9cbf9f7f0747b789512f4de93fba590214774409
yusuke <yusuke.iritani0909@gmail.com>
2025-01-01 13:41:32 +0900

    connectで簡単なget

---
+++ apps/connect-go-example/go.mod
@@ -0,0 +1,13 @@
+module example
+
+go 1.23.4
+
+require (
+	connectrpc.com/connect v1.17.0
+	connectrpc.com/cors v0.1.0
+	github.com/rs/cors v1.11.1
+	golang.org/x/net v0.33.0
+	google.golang.org/protobuf v1.36.1
+)
+
+require golang.org/x/text v0.21.0 // indirect
--- apps/grpc_frontend/src/pages/index.tsx
+++ apps/grpc_frontend/src/pages/index.tsx
@@ -5,13 +5,27 @@ import { userFetcher } from '@/hooks/user-hook';
 import useSWR from 'swr';
 import Users from '@/components/users';
 import Orders from '@/components/orders';
+import { createClient } from '@connectrpc/connect';
+import { createConnectTransport } from '@connectrpc/connect-web';
+import { PingService } from '@/gen/ping_pb';
+const transport = createConnectTransport({
+  baseUrl: 'http://localhost:8080',
+});
+const client = createClient(PingService, transport);
 
 export default function Home() {
   const [users, setUsers] = useState<User[]>([]);
-  const { data, error } = useSWR<User[]>(`/api/user`, userFetcher);
-  if (data && users.length === 0) {
-    setUsers(data);
-  }
+  // const { data, error } = useSWR<User[]>(`/api/user`, userFetcher);
+  // if (data && users.length === 0) {
+  //   setUsers(data);
+  // }
+
+  const userFetcher = async () => {
+    const data = await client.ping({}).then((res) => {
+      console.log(res);
+    });
+    console.log(data);
+  };
 
   return (
     <>
@@ -24,18 +38,19 @@ export default function Home() {
         </h1>
       </div>
       <div className="flex flex-col h-screen">
-        {error && (
-          <p className="text-red-500 text-center">Error loading data</p>
-        )}
+        {/*{error && (*/}
+        {/*  <p className="text-red-500 text-center">Error loading data</p>*/}
+        {/*)}*/}
         <div className="flex-2 bg-red-200 flex items-start justify-start pl-10 pt-10">
-          <Ping />
+          {/*<Ping />*/}
+          <button onClick={userFetcher}>Fetch</button>
         </div>
-        <div className="flex-2 bg-green-200 flex items-start justify-start pl-10 pt-10">
-          <Users users={users} />
-        </div>
-        <div className="flex-1 bg-blue-200 flex items-start justify-start pl-10 pt-10">
-          <Orders users={users} />
-        </div>
+        {/*<div className="flex-2 bg-green-200 flex items-start justify-start pl-10 pt-10">*/}
+        {/*  <Users users={users} />*/}
+        {/*</div>*/}
+        {/*<div className="flex-1 bg-blue-200 flex items-start justify-start pl-10 pt-10">*/}
+        {/*  <Orders users={users} />*/}
+        {/*</div>*/}
       </div>
     </>
   );
