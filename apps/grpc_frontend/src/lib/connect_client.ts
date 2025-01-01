0c4a7209ebb2d8f3bdd56ce7530a300fadee0ee7
yusuke <yusuke.iritani0909@gmail.com>
2025-01-02 02:11:41 +0900

    front env

--- apps/grpc_frontend/src/lib/connect_client.ts
+++ apps/grpc_frontend/src/lib/connect_client.ts
@@ -1,5 +1,5 @@
 import { createConnectTransport } from '@connectrpc/connect-web';
 
 export const transport = createConnectTransport({
-  baseUrl: process.env.NEXT_PUBLIC_BACKEND_URL || 'env not set',
+  baseUrl: process.env.NEXT_PUBLIC_API_URL || 'env not set',
 });
